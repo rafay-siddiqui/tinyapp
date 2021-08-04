24597ac6da786deba0f41305f820fad2f27350c4
Rafay Siddiqui <rafays.siddiqui@gmail.com>
2021-08-04 10:07:02 +0000

    rendered variables and created a list for index page

--- express_server.js
+++ express_server.js
@@ -11,7 +11,16 @@ const urlDatabase = {
 };
 
 app.get("/", (req, res) => {
-  res.render('pages/index');
+  var mascots = [
+    { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012 },
+    { name: 'Tux', organization: "Linux", birth_year: 1996 },
+    { name: 'Moby Dock', organization: "Docker", birth_year: 2013 }
+  ];
+  var tagline = "No programming concept is complete without a cute animal mascot.";
+  res.render('pages/index', {
+    mascots: mascots,
+    tagline: tagline
+  });
 });
 
 app.get("/about", (req, res) => {
