e119c4159ffbc4ded44e5543312657bd38784764
Rafay Siddiqui <rafays.siddiqui@gmail.com>
2021-08-04 21:25:34 +0000

    Added a urls_index.ejs in views directory, cleaned up express_server.js from code used to learn templating and routing

--- express_server.js
+++ express_server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
+const PORT = 8080;
 
 app.set('view engine', 'ejs');
 
-const PORT = 8080;
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -25,19 +25,6 @@ app.get("/", (req, res) => {
 
 app.get("/about", (req, res) => {
   res.render('pages/about');
-});
-
-app.get('/hello', (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.get('/set', (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
-
-app.get('/fetch', (req, res) => {
-  res.send(`a = ${a}`);
 });
 
 app.listen(PORT, () => {
